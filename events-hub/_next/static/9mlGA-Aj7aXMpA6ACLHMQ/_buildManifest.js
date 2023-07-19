47f61a8e3c077b2b27334620f7b92825a6834881
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2023-07-19 17:19:00 +0530

    Add events-hub files

---
+++ events-hub/_next/static/9mlGA-Aj7aXMpA6ACLHMQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/32cc4e91-f7927a2ff25028fb.js","static/chunks/pages/index-617905a34f9c2442.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
